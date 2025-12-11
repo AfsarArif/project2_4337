51c962286796fb2eaac64e9554a56b5539e58b34
AfsarArif <Mohamedafsar.arif@gmail.com>
2025-12-10 19:44:01 -0600

    v1.1

--- maze.pl
+++ maze.pl
@@ -6,3 +6,26 @@ find_exit(Maze, Actions) :-
     start_position(Maze, SR, SC),
     follow_actions(Maze, SR, SC, Actions, ER, EC),
     cell_at(Maze, ER, EC, e).
+
+valid_maze(Maze) :-
+    is_list(Maze),
+    Maze \= [],
+
+    % all rows must be lists
+    maplist(is_list, Maze),
+
+    % ensure rectangular
+    Maze = [Row0 | _],
+    length(Row0, Cols),
+    Cols > 0,
+    maplist(same_length(Cols), Maze),
+
+    % exactly one start
+    findall((R,C), cell_at(Maze, R, C, s), Starts),
+    length(Starts, 1),
+
+    % at least one exit
+    findall((R,C), cell_at(Maze, R, C, e), Exits),
+    Exits \= [].
+
+same_length(N, Row) :- length(Row, N).
